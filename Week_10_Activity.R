d4870ace7062f0535b32d8a52406ea3faafc1e1a
Benny Pagett <bennypagett@gmail.com>
2021-04-26 13:57:16 +0200

    upload of activity 10 files

---
+++ Week_10_Activity.R
@@ -0,0 +1,14 @@
+### Week 10 
+
+install.packages("car")
+
+
+library(tidyverse)
+library(readr)
+library(dplyr)
+library(broom)
+library(car)
+library(psych)
+library(performance)
+
+?performance::check_model
